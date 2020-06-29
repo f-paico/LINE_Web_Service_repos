667914299328a9dabba8054981ccd2cd19e9039d
kaida <cakeandkta@gmail.com>
2020-06-29 19:13:01 +0900

    fix easily

--- static/index_v2.js
+++ static/index_v2.js
@@ -8,8 +8,8 @@ const config = {
 
 function showCalendar(year, month){
   for (i = 0; i < config.show; i++){
-    const calendarHtml = createCalendar(year, month)
-    const sec = document.createElement('section')
+    let calendarHtml = createCalendar(year, month)
+    let sec = document.createElement('section')
     sec.innerHTML = calendarHtml
     console.log(sec.innerHTML)
     document.querySelector('#calendar').appendChild(sec)
